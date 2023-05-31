37c94b40fd783cdad2b579a7ae9f76c9cd119323
minheejung <dark535@naver.com>
2023-05-31 09:26:29 +0900

    23-05-31

--- kafka/pro.py
+++ kafka/pro.py
@@ -5,9 +5,12 @@ import pandas as pd
 server = "yourip:9092"
 abalone_topic = "abalone"
 
-producer = KafkaProducer(
-    bootstrap_servers=[server],
-    value_serializer=lambda x: json.dumps(x).encode("utf-8")
+producer = KafkaProducer(ask=0, #메시지 받은 사람이 메시지를 잘 받았는지 체크하는 옵션 (0은 그냥 보내기만 한다. 확인x)
+    compression_type='gzip',#메시지 전달할 때 압축
+    bootstrap_servers=[server] #전달하고자하는 카프카 브로커의 위치,
+    value_serializer=lambda x: json.dumps(x).encode("utf-8") #직렬화 : 데이터 전송을 위해 byte단위로 바꿔주는 작업 : 
+                                                               #dumps 함수이용. dump : json 값을 메모리에 올려준다. encode를 통해서 올린다.
+                                                               #x가 있으면, x를 dumps로 바꾸고 encode 한다.
 )
 
 df = pd.read_csv("/home/ubuntu/abalone.csv")
@@ -27,4 +30,4 @@ for _, row in df.iterrows():
 
     producer.send(abalone_topic, value=message)
 
-producer.flush()
+producer.flush() # 비우는 작업.
